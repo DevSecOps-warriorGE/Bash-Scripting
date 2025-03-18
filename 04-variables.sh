2b1b4fbd46b5e156affeb412161610ed0170983d
ganeshaeshwar09 <ganeshaeshwar09@gmail.com>
2025-03-18 19:52:55 +0530

    Variables-very important in scripting

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+a=10
+echo "Printing the value of a $a"
+echo "Printing the value of b $b"
+# When you attempt to print the value of a variable that is not defined, it will print the empty value. 
+
+
+# $a or ${a} or "${a}" 
+echo "Printing the value of a $a"
+echo "Printing the value of a ${a}"
+echo Printing the value of a ${a}
+echo 'Printing the value of a ${a}'
+# when you enclose any variable in single quotes, it will print the variable name as it is. That also means, single quotes will elimiate the special variables power.
