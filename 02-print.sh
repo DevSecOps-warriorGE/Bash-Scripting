5f330b5195e774c22efde344f170fefda6341aff
ganeshaeshwar09 <ganeshaeshwar09@gmail.com>
2025-03-18 18:43:18 +0530

    updated the shebang.md

--- 02-print.sh
+++ 02-print.sh
@@ -1,7 +1,7 @@
 echo "I want to land in FAANG"
 
-echo -e "Microsoft /n Google /n AWS /n Netflix"
+echo -e "Microsoft \n Google \n AWS \n Netflix"
 
-echo -e "Australia\n\tLondon\n\tLos Angeles"
+echo -e "\tAustralia\n\tLondon\n\tLos Angeles"
 
-echo -e "Somehow\t\t\tdo\t\t\tit"
+echo -e "Somehow\t\tdo\t\tit"
