9917f93eeaa4ff09023a001350919ff8f7a1ab6c
ganeshaeshwar09 <ganeshaeshwar09@gmail.com>
2025-03-18 20:11:16 +0530

    Updating date in scripts

--- 04-variables.sh
+++ 04-variables.sh
@@ -10,3 +10,8 @@ echo "Printing the value of a ${a}"
 echo Printing the value of a ${a}
 echo 'Printing the value of a ${a}'
 # when you enclose any variable in single quotes, it will print the variable name as it is. That also means, single quotes will elimiate the special variables power.
+
+
+TODAYS_DATE=$(date +%F)
+echo "Today's date is $TODAYS_DATE"
+
